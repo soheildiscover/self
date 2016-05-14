fdbbac1a83476715ef2532a1783cf915ce55281f
MrTorento <seyedalikia27@gmail.com>
2016-05-14 13:32:38 +0430

    Update quran.lua

--- plugins/quran.lua
+++ plugins/quran.lua
@@ -1,50 +1,53 @@
 do
-umbrella_cp = "http://umbrella.shayan-soft.ir/quran/"
+umbrella = "http://umbrella.shayan-soft.ir/quran/" -- database
 
-
+-- get sound of sura
 local function read_sura(chat_id, target)
-local readq = http.request(umbrella_cp.."Sura"..target..".mp3")
-local url = umbrella_cp.."Sura"..target..".mp3"
-local file = download_to_file(url, umbrella_cp.mp3)
-send_document("chat#id"..chat_id, file, ok_cb, false)
+	local readq = http.request(umbrella.."Sura"..target..".mp3")
+	local url = umbrella.."Sura"..target..".mp3"
+	local file = download_to_file(url)
+	local cb_extra = {file_path=file}
+	return send_document("chat#id"..chat_id, file, rmtmp_cb, cb_extra)
 end
 
-
+-- get text of sura
 local function view_sura(chat_id, target)
-local viewq = http.request(umbrella_cp.."quran ("..target..").txt")
-viewq = string.gsub(viewq, "@UmbrellaTeam", " ")
-viewq = string.gsub(viewq, " K  ", "@ ")
-return viewq
+	local viewq = http.request(umbrella.."quran ("..target..").txt")
+	return viewq
 end
 
+-- run script
 local function run(msg, matches)
     local chat_id = msg.to.id
-if matches[1] == "ead" then
-local target = matches[2]
-return read_sura(chat_id, target)
-elseif matches[1] == "ura" then
-local target = matches[2]
-return view_sura(chat_id, target)
-elseif matches [1] == "uran" then
-local qlist = http.request(umbrella_cp.."list.txt") 
-qlist = string.gsub(qlist, "@UmbrellaTeam", "@CopierTeam")
-return qlist
-end
+	
+	if matches[1] == "read" then
+		local target = matches[2]
+		return read_sura(chat_id, target)
+		
+	elseif matches[1] == "sura" then
+		local target = matches[2]
+		return view_sura(chat_id, target)
+		
+	elseif matches [1] == "quran" then
+		local qlist = http.request(umbrella.."list.txt") -- list of suras
+		return qlist
+		
+	end
 end
 
+-- other help and commands
 return {
-description = "umbrella_cp Quran Project", 
-usage = {
-"sura (num) : view arabic sura",
-"read (num) : send sound of sura",
-"quran : sura list of quran",
-},
-patterns = {
-"^[Ss](ura) (.+)$",
-"^[Rr](ead) (.+)$",
-"^[Qq](uran)$",
-}, 
-run = run,
-}
-
+	description = "Umbrella Quran Project", 
+	usage = {
+		"!sura (num) : view arabic sura",
+		"!read (num) : send sound of sura",
+		"!quran : sura list of quran",
+		},
+	patterns = {
+		"^[!/](sura) (.+)$",
+		"^[!/](read) (.+)$",
+		"^[!/](quran)$",
+		}, 
+	run = run,
+	}
 end

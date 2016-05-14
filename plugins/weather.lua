26bfa87afe745eae8ab219bbd2f86bd413d1e4c0
MrTorento <seyedalikia27@gmail.com>
2016-05-14 13:13:24 +0430

    Update weather.lua

--- plugins/weather.lua
+++ plugins/weather.lua
@@ -13,8 +13,8 @@ local function get_weather(location)
   local weather = json:decode(b)
   local city = weather.name
   local country = weather.sys.country
-  local temp = 'دمای شهر '..city..' الان '..weather.main.temp ..درجه سانتی گراد هست!''
-  local conditions = 'وضع آب و هواش  : '
+  local temp = 'دمای شهر '..city..' هم اکنون '..weather.main.temp..' درجه سانتی گراد می باشد'
+  local conditions = 'شرایط فعلی آب و هوا : '
 
   if weather.weather[1].main == 'Clear' then
     conditions = conditions .. 'آفتابی ☀'
